18d97a60ee1d4d0a6c41bc43fc355841dbd4b728
SUFFOSOHRAOUL <70449108+SUFFOSOHRAOUL@users.noreply.github.com>
2022-06-24 09:23:54 -0700

    null-aware identifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ void main() {
   runApp(const MyApp());
 }
 
-void test() {
-  const String? firstName = null;
-  const String? middleName = null;
-const String? lastName = 'raoul'; 
+void test(String? firstName, String? middleName, String lastName) {
+  
 
-const firstNonNullValue = (firstName?? middleName )?? lastName;
-print(firstNonNullValue);
+ String? name = firstName;
+name ??= middleName;
+name ??= lastName;
+print(name);
   
 }
 class MyApp extends StatelessWidget {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    test();
+    test(null,null,'hans');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
